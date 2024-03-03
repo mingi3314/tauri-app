c9679660f45d06d2257f81ac5c1edb6849be6596
Minki Kim <mingi3314@gmail.com>
2024-03-03 14:35:04 +0900

    ✨ add account status check button

--- src/components/Greet.vue
+++ src/components/Greet.vue
@@ -1,9 +1,23 @@
 <script setup>
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
+import axios from 'axios';
 
 const greetMsg = ref("");
 const name = ref("");
+
+const responseMsg = ref("");
+
+async function getAccount() {
+  try {
+    const response = await axios.get("http://localhost:8000/accounts/default");
+    const { brokerage } = response.data.account;
+    responseMsg.value = `${brokerage} 계좌에 연결되었습니다.`;
+  } catch (error) {
+    console.error("There was an error:", error);
+    responseMsg.value = "계좌 연결 상태를 가져오는 데 실패했습니다.";
+  }
+}
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
@@ -18,4 +32,7 @@ async function greet() {
   </form>
 
   <p>{{ greetMsg }}</p>
+
+  <button @click="getAccount">계좌 연결 상태</button>
+  <p>{{ responseMsg }}</p>
 </template>
