8bff73b9667291433f74141d5fa56c2d4db80621
Minki Kim <mingi3314@gmail.com>
2024-03-19 00:44:19 +0900

    ✨ Refactor modal components and add Rebalance component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <script setup>
 import { ref } from 'vue';
-import AccountModal from './components/AccountModal.vue';
+import Modal from './components/Modal.vue';
 import Account from './components/Account.vue';
+import Rebalance from './components/Rebalance.vue';
 import PortfolioTable from './components/PortfolioTable.vue';
 
-const isModalOpen = ref(false);
 
-function openModal() {
-  isModalOpen.value = true;
+const isAccountModalOpen = ref(false);
+const isRebalanceModalOpen = ref(false);
+
+function openAccountModal() {
+  isAccountModalOpen.value = true;
 }
 
-function closeModal() {
-  isModalOpen.value = false;
+function closeAccountModal() {
+  isAccountModalOpen.value = false;
+}
+
+function openRebalanceModal() {
+  isRebalanceModalOpen.value = true;
+}
+
+function closeRebalanceModal() {
+  isRebalanceModalOpen.value = false;
 }
 </script>
 
@@ -19,12 +30,17 @@ function closeModal() {
   <div id="app">
 
     <div class="button-container">
-      <button @click="openModal">계좌 등록하기</button>
+      <button @click="openAccountModal">계좌 등록하기</button>
+      <button @click="openRebalanceModal">리밸런싱 실행</button>
     </div>
 
-    <AccountModal v-if="isModalOpen" @on-backdrop-clicked="closeModal" ref="accountModal">
-      <Account @btn-close-clicked="closeModal" />
-    </AccountModal>
+    <Modal v-if="isAccountModalOpen" @on-backdrop-clicked="closeAccountModal" ref="accountModal">
+      <Account @btn-close-clicked="closeAccountModal" />
+    </Modal>
+
+    <Modal v-if="isRebalanceModalOpen" @on-backdrop-clicked="closeRebalanceModal" ref="rebalanceModal">
+      <Rebalance @btn-close-clicked="closeRebalanceModal" />
+    </Modal>
 
     <PortfolioTable />
   </div>
@@ -37,5 +53,10 @@ function closeModal() {
   /* 우측 정렬 */
   padding: 20px;
   /* 상단 및 우측 여백 */
+}
+
+.button-container button {
+  margin-left: 20px;
+  /* 버튼 사이 여백 */
 }
 </style>
