8de021735ab4362d1f113ffa331c9795527a50fb
Minki Kim <mingi3314@gmail.com>
2024-03-20 01:32:35 +0900

    💄 Add PrimeVue and apply TreeTable

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import App from "./App.vue";
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faChevronRight, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
 
 library.add(faChevronRight, faChevronDown);
 
 const app = createApp(App);
 
 app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(PrimeVue)
 
 app.mount('#app');
