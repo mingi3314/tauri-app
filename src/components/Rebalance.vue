024377ce6d045ffcf81c152694c07188f99b4932
Minki Kim <mingi3314@gmail.com>
2024-03-24 01:06:29 +0900

    ♻️ template, script 위치 변경

--- src/components/Rebalance.vue
+++ src/components/Rebalance.vue
@@ -1,3 +1,36 @@
+<template>
+    <div>
+        <h2>주문 확인</h2>
+        <div v-if="orders.length">
+            <table>
+                <!-- 테이블 헤더 -->
+                <thead>
+                    <tr>
+                        <th>상품</th>
+                        <th>가격</th>
+                        <th>수량</th>
+                        <th>매매 유형</th>
+                    </tr>
+                </thead>
+                <!-- 테이블 바디 -->
+                <tbody>
+                    <tr v-for="order in orders" :key="order.symbol">
+                        <td>{{ order.symbol }}</td>
+                        <td>{{ order.price }}</td>
+                        <td>{{ order.quantity }}</td>
+                        <td>{{ order.side }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-else>주문 불러오는중...</div>
+        <div class="button-group">
+            <button @click="btnCloseClicked">닫기</button>
+            <button @click="placeOrders">주문 실행</button>
+        </div>
+    </div>
+</template>
+
 <script setup>
 import { ref, reactive } from "vue";
 import axios from 'axios';
@@ -31,35 +64,3 @@ function btnCloseClicked() {
 </script>
 
 
-<template>
-    <div>
-        <h2>주문 확인</h2>
-        <div v-if="orders.length">
-            <table>
-                <!-- 테이블 헤더 -->
-                <thead>
-                    <tr>
-                        <th>상품</th>
-                        <th>가격</th>
-                        <th>수량</th>
-                        <th>매매 유형</th>
-                    </tr>
-                </thead>
-                <!-- 테이블 바디 -->
-                <tbody>
-                    <tr v-for="order in orders" :key="order.symbol">
-                        <td>{{ order.symbol }}</td>
-                        <td>{{ order.price }}</td>
-                        <td>{{ order.quantity }}</td>
-                        <td>{{ order.side }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        <div v-else>주문 불러오는중...</div>
-        <div class="button-group">
-            <button @click="btnCloseClicked">닫기</button>
-            <button @click="placeOrders">주문 실행</button>
-        </div>
-    </div>
-</template>
